abefb579baeb09d4a785c7a9842d84828b41a1dd
carmen52 <loubecwith@gmail.com>
2014-12-03 21:28:58 -0500

    1

---
+++ Carmen52.java
@@ -0,0 +1,30 @@
+
+
+//carmen52
+import java.util.Scanner;
+
+public class Carmen52
+{
+public static void main(String[] args) 
+{
+Scanner Keyboard = new Scanner(System.in);
+
+System.out.println("Enter X"); 
+
+int x = Keyboard.nextInt();
+
+System.out.println("Enter Y"); 
+
+int y = Keyboard.nextInt(); 
+
+double ans = 0; 
+
+while (x<=y) 
+{
+	 ans = 1/(Math.sqrt(x)); 
+	 x++; 
+
+}
+System.out.println(ans); 
+} 
+} 
